db720eade20955a0a95252859d0351e2ff47a223
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-06 19:14:28 +0530

    Improve Captcha controller

--- application/controllers/captcha.php
+++ application/controllers/captcha.php
@@ -4,60 +4,40 @@
  * CAPTCHA CONTROLLER
  *
  * @package    FCstuff
+ * @category   Authentication
  * @author     Abhijit Parida
  * @copyright  Copyright (c) 2013, FCstuff
  */
 class Captcha extends CI_Controller {
 
     /**
-     * Constructor.
+     * Output captcha image and set the captcha string as a session variable.
      *
      * @access  public
-     * @return  void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        // Load string helper.
-        // Required for generating random string.
-        $this->load->helper('string');
-    }
-
-    /**
-     * Return captcha image and set the captcha string as a session variable.
-     *
-     * @access  public
-     * @return  image/png
      */
     public function index()
     {
         // Generate captcha text.
+        $this->load->helper('string');
         $text = strtolower(random_string('alpha', 5));
         $this->session->set_userdata('captcha', $text);
 
-        // Set font path.
-        $font = 'application/assets/fonts/captcha.ttf';
-
-        // Create a 125 x 60 image.
+        // Create image.
         $image = imagecreatetruecolor(125, 60);
         imagesavealpha($image, true);
-
-        // Set font color.
-        $color = imagecolorallocate($image, 39, 97, 155);
 
         // Set image background.
         $back = imagecolorallocatealpha($image, 0, 0, 0, 127);
         imagefill($image, 0, 0, $back);
 
         // Add captcha text.
+        $font = 'application/assets/fonts/captcha.ttf';
+        $color = imagecolorallocate($image, 39, 97, 155);
         imagettftext($image, 40, 0, 5, 40, $color, $font, $text);
 
-        // Return captcha.
+        // Output image.
         header("Content-type: image/png");
         imagepng($image);
-
-        // Free up memory.
         imagedestroy($image);
     }
 }
