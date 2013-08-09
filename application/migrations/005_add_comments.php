b9e3aa76dd801c46d73612b68995f0e9ddb9cbf1
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-09 12:33:08 +0530

    Add migrations for Comments table

---
+++ application/migrations/005_add_comments.php
@@ -0,0 +1,49 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Migrations for 'comments' table.
+ *
+ * @package    FCstuff
+ * @category   Database
+ * @author     Abhijit Parida
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+class Migration_Add_comments extends CI_Migration {
+
+    /**
+     * Create 'comments' table.
+     *
+     * @access  public
+     */
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            "comment_id INT(10) NOT NULL AUTO_INCREMENT",
+            "post_id INT(10)",
+            "user_id INT(10)",
+            "content TEXT",
+            "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+        ));
+
+        $this->dbforge->add_key('comment_id', TRUE);
+        $this->dbforge->add_key('post_id');
+        $this->dbforge->add_key('user_id');
+
+        $this->dbforge->create_table('comments');
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Drop 'comments' table.
+     *
+     * @access  public
+     */
+    public function down()
+    {
+        $this->dbforge->drop_table('comments');
+    }
+}
+
+/* End of file 005_add_comments.php */
+/* File location : ./application/migrations/005_add_comments.php */
