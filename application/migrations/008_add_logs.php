c378ccc2301004e80d674db1861ccf0d73b8d01c
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-09 21:30:26 +0530

    Add migrations for Logs table

---
+++ application/migrations/008_add_logs.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Migrations for 'logs' table.
+ *
+ * @package    FCstuff
+ * @category   Database
+ * @author     Abhijit Parida
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+class Migration_Add_logs extends CI_Migration {
+
+    /**
+     * Create 'logs' table.
+     *
+     * @access  public
+     */
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            "sl INT(10) NOT NULL AUTO_INCREMENT",
+            "user_id INT(10)",
+            "controller VARCHAR(30)",
+            "view VARCHAR(30)",
+            "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP",
+            "ip_address VARCHAR(15)",
+            "user_agent VARCHAR(300)"
+        ));
+
+        $this->dbforge->add_key('sl', TRUE);
+        $this->dbforge->add_key('user_id');
+
+        $this->dbforge->create_table('logs');
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Drop 'logs' table.
+     *
+     * @access  public
+     */
+    public function down()
+    {
+        $this->dbforge->drop_table('logs');
+    }
+}
+
+/* End of file 008_add_logs.php */
+/* File location : ./application/migrations/008_add_logs.php */
