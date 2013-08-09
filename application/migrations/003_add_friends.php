ca2fab7b402a7efa5d25e980d5924e2f051a4bf4
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-09 12:17:26 +0530

    Add migrations for Friends table

---
+++ application/migrations/003_add_friends.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Migrations for 'friends' table.
+ *
+ * @package    FCstuff
+ * @category   Database
+ * @author     Abhijit Parida
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+class Migration_Add_friends extends CI_Migration {
+
+    /**
+     * Create 'friends' table.
+     *
+     * @access  public
+     */
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            "user_id INT(10)",
+            "friend_id INT(10)",
+            "status VARCHAR(10)",
+            "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+        ));
+
+        $this->dbforge->add_key('user_id');
+        $this->dbforge->add_key('friend_id');
+
+        $this->dbforge->create_table('friends');
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Drop 'friends' table.
+     *
+     * @access  public
+     */
+    public function down()
+    {
+        $this->dbforge->drop_table('friends');
+    }
+}
+
+/* End of file 003_add_friends.php */
+/* File location : ./application/migrations/003_add_friends.php */
