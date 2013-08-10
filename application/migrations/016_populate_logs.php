5cce10f4af8bebaf268f234ab1ec5e1ff0588c1e
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-10 19:48:08 +0530

    Add migrations to populate Logs table

---
+++ application/migrations/016_populate_logs.php
@@ -0,0 +1,53 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Migrations for populating 'logs' table.
+ *
+ * @package    FCstuff
+ * @category   Database
+ * @author     Abhijit Parida
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+class Migration_Populate_logs extends CI_Migration {
+
+    /**
+     * Populate 'logs' table.
+     *
+     * @access  public
+     */
+    public function up()
+    {
+        $ip_address = $this->session->userdata('ip_address');
+        $user_agent = $this->session->userdata('user_agent');
+
+        for ($i = 0; $i < 5 ; $i++)
+        {
+            // User
+            for ($j = 2; $j <= 6 ; $j++)
+            {
+                $this->db->insert('logs', array(
+                    'user_id'    => $j,
+                    'controller' => 'Example Controller',
+                    'view'       => 'Example View',
+                    'ip_address' => $ip_address,
+                    'user_agent' => $user_agent
+                ));
+            }
+        }
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Empty 'logs' table.
+     *
+     * @access  public
+     */
+    public function down()
+    {
+        $this->db->empty_table('logs');
+    }
+}
+
+/* End of file 016_populate_logs.php */
+/* File location : ./application/migrations/016_populate_logs.php */
