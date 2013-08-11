db66e28b027042c750b0d7e15ad7030dcd81fb1b
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-11 19:11:19 +0530

    Add migrations to populate Ban table

---
+++ application/migrations/018_populate_ban.php
@@ -0,0 +1,48 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Migrations for populating 'ban' table.
+ *
+ * @package    FCstuff
+ * @category   Database
+ * @author     Abhijit Parida
+ * @license    The MIT License (MIT)
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+class Migration_Populate_ban extends CI_Migration {
+
+    /**
+     * Populate 'ban' table.
+     *
+     * @access  public
+     */
+    public function up()
+    {
+        // User_id
+        for ($i = 5; $i <= 6 ; $i++)
+        {
+            $data = array(
+                'user_id'   => $i,
+                'offence'   => 'Sample banning.',
+                'initiator' => 'Admin'
+            );
+            $this->db->set('expire', 'NOW() + INTERVAL 1 HOUR', FALSE);
+            $this->db->insert('ban', $data);
+        }
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Empty 'ban' table.
+     *
+     * @access  public
+     */
+    public function down()
+    {
+        $this->db->empty_table('ban');
+    }
+}
+
+/* End of file 018_populate_ban.php */
+/* File location : ./application/migrations/018_populate_ban.php */
