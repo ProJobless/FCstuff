6d4a352a8a40f1dd170dcef28067d98cb57c7c57
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-11 13:26:33 +0530

    Add User model

---
+++ application/models/user.php
@@ -0,0 +1,99 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * USER MODEL
+ *
+ * @package    FCstuff
+ * @category   User
+ * @author     Abhijit Parida
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+
+class User extends CI_Model {
+
+    /**
+     * Create user.
+     *
+     * @access   public
+     * @param    array
+     */
+    public function create($user)
+    {
+        $this->db->insert('users', $user);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+    * Return user data.
+    *
+    * @access   public
+    * @param    mixed
+    * @return   array
+    */
+   public function read($identifier)
+   {
+        $this->db->from('users');
+        $this->db->select('');
+        $this->db->limit(1);
+        $this->db->where('user_id', $identifier);
+        $this->db->or_where('username', $identifier);
+        $this->db->or_where('email', $identifier);
+        $query = $this->db->get();
+
+        return $query->result_array();
+   }
+
+   // --------------------------------------------------------------------
+
+   /**
+    * Update user data.
+    *
+    * @access   public
+    * @param    int
+    * @param    array
+    */
+   public function update($user_id, $data)
+   {
+        $this->db->where('user_id', $user_id);
+        $this->db->update('users', $data);
+   }
+
+   // --------------------------------------------------------------------
+
+   /**
+    * Delete user.
+    *
+    * @access   public
+    * @param    int
+    */
+   public function delete($user_id)
+   {
+        $this->db->where('user_id', $user_id);
+        $this->db->delete('users');
+   }
+
+   // --------------------------------------------------------------------
+
+   /**
+    * Search users.
+    *
+    * @access   public
+    * @param    string
+    * @param    int
+    * @param    int
+    * @return   array
+    */
+   public function search($name, $limit = 5, $offset = 0)
+   {
+        $this->db->from('users');
+        $this->db->like('name', $name);
+        $this->db->limit($limit, $offset);
+        $query = $this->db->get();
+
+        return $query->result_array();
+   }
+}
+
+/* End of file user.php */
+/* File location : ./application/models/user.php */
