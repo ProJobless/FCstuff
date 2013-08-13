d11b0545ceb07aa56c0d84ac5f3bf53ca8398327
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-13 20:22:18 +0530

    Add Cookie model

---
+++ application/models/cookie.php
@@ -0,0 +1,80 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * COOKIE MODEL
+ *
+ * @package    FCstuff
+ * @category   Content
+ * @author     Abhijit Parida
+ * @license    The MIT License (MIT)
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+
+class Cookie extends CI_Model {
+
+    /**
+     * Add a new authentication token.
+     *
+     * @access   public
+     * @param    array
+     */
+    public function create($cookie)
+    {
+        $this->db->insert('cookies', $cookie);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Return details about an authentication token.
+     *
+     * @access   public
+     * @param    int
+     * @param    int
+     * @return   array
+     */
+    public function read($user_id, $token)
+    {
+        $this->from('cookies');
+        $this->db->select('');
+        $this->db->limit(1);
+        $this->db->where('user_id', $user_id);
+        $this->db->where('token', $token);
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Update an existing authentication token.
+     *
+     * @access   public
+     * @param    int
+     * @param    array
+     */
+    public function update($cookie_id, $data)
+    {
+        $this->db->where('cookie_id', $cookie_id);
+        $this->db->limit(1);
+        $this->db->update('cookies', $data);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Delete an authentication token.
+     *
+     * @access   public
+     * @param    int
+     */
+    public function delete($cookie_id)
+    {
+        $this->db->where('cookie_id', $cookie_id);
+        $this->db->delete('cookies');
+    }
+}
+
+/* End of file cookie.php */
+/* File location : ./application/models/cookie.php */
