05b0b05346f43d25554b46262d775b15783b30bc
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-14 18:14:11 +0530

    Add Rating model

---
+++ application/models/rating.php
@@ -0,0 +1,63 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * RATING MODEL
+ *
+ * @package    FCstuff
+ * @category   Content
+ * @author     Abhijit Parida
+ * @license    The MIT License (MIT)
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+
+class Rating extends CI_Model {
+
+    /**
+     * Add a new rating.
+     *
+     * @access   public
+     * @param    array
+     */
+    public function create($rating)
+    {
+        $this->db->insert('ratings', $rating);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Return ratings for a post / comment.
+     *
+     * @access   public
+     * @param    string
+     * @param    int
+     */
+    public function read($content_type, $content_id)
+    {
+        $this->db->from('ratings');
+        $this->db->select('');
+        $this->db->where('type', $content_type);
+        $this->db->where('content_id', $content_id);
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Update an existing rating.
+     *
+     * @access   public
+     * @param    int
+     * @param    array
+     */
+    public function update($rating_id, $rating)
+    {
+        $this->db->where('rating_id', $rating_id);
+        $this->db->update('ratings', $rating);
+    }
+}
+
+/* End of file rating.php */
+/* File location : ./application/models/rating.php */
