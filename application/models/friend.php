3821b41159d44a56e7c91ab4a751ad363d06fcc3
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-17 23:20:26 +0530

    Add Friend model

---
+++ application/models/friend.php
@@ -0,0 +1,106 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * FRIEND MODEL
+ *
+ * @package    FCstuff
+ * @category   User
+ * @author     Abhijit Parida
+ * @license    The MIT License (MIT)
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+
+class Friend extends CI_Model {
+
+    /**
+     * Add a new friend.
+     *
+     * @access   public
+     * @param    array
+     */
+    public function create($data)
+    {
+        $this->db->insert('friends', $data);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Return friend data between two users.
+     *
+     * @access   public
+     * @param    int
+     * @param    int
+     * @return   array
+     */
+    public function read($user_id, $friend_id)
+    {
+        $this->db->query("SET time_zone = '+00:00'");
+        $this->db->from('friends');
+        $this->db->select('');
+        $this->db->limit(1);
+        $this->db->where('user_id', $user_id);
+        $this->db->where('friend_id', $friend_id);
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Return list of friends of a user.
+     *
+     * @access   public
+     * @param    int
+     * @param    int
+     * @return   array
+     */
+    public function user($user_id, $last_relationship_id = FALSE)
+    {
+        $this->db->query("SET time_zone = '+00:00'");
+        $this->db->from('friends');
+        $this->db->select('');
+        $this->db->limit(1);
+        $this->db->where('user_id', $user_id);
+        $this->db->where('friend_id', $friend_id);
+        if ($last_relationship_id) {
+            $this->db->where('relationship_id <', $last_relationship_id);
+        }
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Update relationship between two users.
+     *
+     * @access   public
+     * @param    int
+     * @param    array
+     */
+    public function update($relationship_id, $data)
+    {
+        $this->db->where('relationship_id', $relationship_id);
+        $this->db->update('friends', $data);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Delete relationship between two users.
+     *
+     * @access   public
+     * @param    int
+     */
+    public function delete($relationship_id)
+    {
+        $this->db->where('relationship_id', $relationship_id);
+        $this->db->delete('friends');
+    }
+}
+
+/* End of file friend.php */
+/* File location : ./application/models/friend.php */
