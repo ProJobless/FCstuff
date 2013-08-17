9cd5285dc0cde82ff54de7539ab2b6ecf6ea4861
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-17 22:46:42 +0530

    Add Conversation model

---
+++ application/models/conversation.php
@@ -0,0 +1,57 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CONVERSATION MODEL
+ *
+ * @package    FCstuff
+ * @category   Conversation
+ * @author     Abhijit Parida
+ * @license    The MIT License (MIT)
+ * @copyright  Copyright (c) 2013, FCstuff
+ */
+
+class Conversation extends CI_Model {
+
+    /**
+     * Add a new message.
+     *
+     * @access   public
+     * @param    array
+     */
+    public function create($message)
+    {
+        $this->db->insert('conversations', $message);
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Return conversation between two users.
+     *
+     * @access   public
+     * @param    int
+     * @param    int
+     */
+    public function read($user_id, $friend_id, $before = FALSE, $after = FALSE)
+    {
+        $this->db->query("SET time_zone = '+00:00'");
+        $this->db->from('conversations');
+        $this->db->select();
+        $this->db->order_by('message_id', 'desc');
+        $this->db->limit(15);
+        $this->db->where('user_id', $user_id);
+        $this->db->where('friend_id', $friend_id);
+        if ($before) {
+            $this->db->where('message_id <', $before);
+        }
+        elseif ($after) {
+            $this->db->where('message_id >', $after);
+        }
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+}
+
+/* End of file conversation.php */
+/* File location : ./application/models/conversation.php */
