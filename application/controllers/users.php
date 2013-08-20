9f30fa228b8dd3170a5cce9e5d99560f7f647c45
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-20 14:00:07 +0530

    Add Create method to Users controller

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -24,6 +24,83 @@ class Users extends CI_Controller {
         $this->load->helper('user');
         $this->load->library('phpass');
     }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Create a new user.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function create($ajax = FALSE)
+    {
+        log_access('users', 'create');
+
+        // Remove user array from $_SESSION.
+        $this->session->unset_userdata('user');
+
+        // Remove authentication cookies.
+        delete_cookie();
+
+        // Grab stuff from $_POST.
+        $name     = $this->input->post('name');
+        $email    = $this->input->post('email');
+        $password = $this->input->post('password');
+        $captcha  = $this->input->post('captcha');
+
+        // Is everything valid?
+        if (is_valid('name', $name)
+            && is_valid('email', $email)
+            && is_valid('password', $password)
+            && is_valid('captcha', $captcha))
+        {
+            $data = array(
+                'username'         => generate_username($email),
+                'email'            => $email,
+                'password'         => $this->phpass->hash($password),
+                'name'             => ucwords(strtolower($name)),
+                'verification_key' => md5(rand()),
+                'recovery_key'     => md5(rand()),
+                'last_seen'        => gmdate('Y-m-d H:i:s')
+            );
+
+            // Add the user to database.
+            $this->user->create($data);
+
+            // Get the user_id.
+            $user = $this->user->read($email);
+            $user_id = $user[0]['user_id'];
+
+            // Set user data as session array.
+            $this->session->set_userdata('user', $user[0]);
+
+            // Invalidate the used captcha.
+            $this->session->unset_userdata('captcha');
+
+            // Generate a profile picture.
+            mkdir('user-content/' . $user_id, 0777, TRUE);
+
+            $this->user->update($user_id, array(
+                'profile_picture' => generate_profile_picture($user_id)
+            ));
+
+            // Set an authentication cookie.
+            set_cookie($user_id);
+
+            // Output TRUE if this is an AJAX request.
+            if ($ajax)
+            {
+                $this->output->set_output(TRUE);
+            }
+        }
+
+        // Redirect to home if this isn't an ajax request.
+        if ( ! $ajax)
+        {
+            redirect('/');
+        }
+    }
 }
 
 /* End of file users.php */
