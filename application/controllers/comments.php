2ff5df0a07b4a6be5291fb12711d5e504ca9e242
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-30 17:55:40 +0530

    Add Create method to Comments controller

--- application/controllers/comments.php
+++ application/controllers/comments.php
@@ -27,6 +27,100 @@ class Comments extends CI_Controller {
         authenticate_cookie();
         try_to_unban();
     }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Handle requests for creating a new comment.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function create($ajax = FALSE)
+    {
+        log_access('comments', 'create');
+
+        $post_id = $this->input->post('post_id');
+        $content = $this->input->post('content');
+
+        $response = $this->_create($post_id, $content);
+
+        if ( ! $ajax)
+        {
+            proceed('posts/' . $post_id);
+        }
+
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($response));
+    }
+
+    /**
+     * Create a new comment.
+     *
+     * @access   private
+     * @param    int       Post id
+     * @param    string    Content
+     * @return   array     Response array
+     */
+    private function _create($post_id, $content)
+    {
+        $response['success'] = FALSE;
+
+        if ( ! ($user = $this->session->userdata('user')))
+        {
+            return $response;
+        }
+
+        if ($user['type'] == 'banned')
+        {
+            return $response;
+        }
+
+        if ( ! ($post = $this->post->read($post_id)))
+        {
+            return $response;
+        }
+
+        if ( ! is_valid('comment', $content))
+        {
+            return $response;
+        }
+
+        $comments = $this->comment->user($user['user_id']);
+
+        if ($comments[0])
+        {
+            $last_comment_time = strtotime($comments[0]['timestamp']);
+            $current_time      = strtotime(gmdate('Y-m-d H:i:s'));
+
+            $diff = $current_time - $last_comment_time;
+
+            if ($diff <= 5)
+            {
+                $response['wait'] = 5 - $diff;
+
+                return $response;
+            }
+        }
+
+        $this->comment->create(array(
+            'post_id' => $post_id,
+            'user_id' => $user['user_id'],
+            'content' => $content
+        ));
+
+        $this->post->update($post_id, array(
+            'last_comment_timestamp' => gmdate('Y-m-d H:i:s'),
+            'comments'               => $post[0]['comments'] + 1
+        ));
+
+        $this->user->update($user['user_id'], array(
+            'reputation' => $user['reputation'] + 2
+        ));
+
+        $response['success'] = TRUE;
+        return $response;
+    }
 }
 
 /* End of file comments.php */
