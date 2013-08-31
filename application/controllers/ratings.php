39b78235faee36cac9f0eceec866ad60928111b0
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-31 21:27:11 +0530

    Add Rate method to Ratings controller

--- application/controllers/ratings.php
+++ application/controllers/ratings.php
@@ -22,10 +22,113 @@ class Ratings extends CI_Controller {
 
         $this->load->model('user');
         $this->load->model('post');
-        $this->load->model('ratings');
+        $this->load->model('rating');
 
         authenticate_cookie();
         try_to_unban();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Handle requests for rating posts.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function rate($ajax = FALSE)
+    {
+        log_access('ratings', 'rate');
+
+        $post_id = $this->input->post('post_id');
+        $rating  = $this->input->post('rating');
+
+        if ($this->_rate($post_id, $rating))
+        {
+            $response['success'] = TRUE;
+        }
+
+        else
+        {
+            $response['success'] = FALSE;
+        }
+
+        if ( ! $ajax)
+        {
+            proceed('posts/' . $post_id);
+        }
+
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($response));
+    }
+
+    /**
+     * Rate content.
+     *
+     * @access   private
+     * @param    int       Post id
+     * @param    int       Rating
+     * @return   bool
+     */
+    private function _rate($post_id, $rating_score)
+    {
+        if ( ! ($user = $this->session->userdata('user')))
+        {
+            return FALSE;
+        }
+
+        if ( ! ($post = $this->post->read($post_id)))
+        {
+            return FALSE;
+        }
+
+        if ( ! is_valid('rating', $rating_score))
+        {
+            return FALSE;
+        }
+
+        $rating = $this->rating->read($user['user_id'], $post_id);
+
+        if ( ! isset($rating[0]))
+        {
+            $this->rating->create(array(
+                'post_id' => $post_id,
+                'user_id' => $user['user_id'],
+                'rating'  => $rating_score
+            ));
+
+            $this->post->update($post_id, array(
+                'rating_score' => $post[0]['rating_score'] + $rating_score,
+                'rating_count' => $post[0]['rating_count'] + 1
+            ));
+
+            $this->user->update($user['user_id'], array(
+                'reputation' => $user['reputation'] + $rating_score
+            ));
+
+            update_session_array();
+
+            $op = $this->user->read($post[0]['user_id']);
+            $this->user->update($post[0]['user_id'], array(
+                'reputation' => $op[0]['reputation'] + $rating_score
+            ));
+        }
+
+        else
+        {
+            $this->rating->update($rating[0]['rating_id'], array(
+                'rating' => $rating_score
+            ));
+
+            $diff = $rating_score - $rating[0]['rating'];
+            $new_rating = $post[0]['rating_score'] + $diff;
+
+            $this->post->update($post_id, array(
+                'rating_score' => $new_rating
+            ));
+        }
+
+        return TRUE;
     }
 }
 
