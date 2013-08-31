b9c7c86b8f0455637b57e0b99127214aff62c59b
Abhijit Parida <abhijitrucks@gmail.com>
2013-08-31 22:41:57 +0530

    Add Delete method to Notifications controller

--- application/controllers/notifications.php
+++ application/controllers/notifications.php
@@ -26,6 +26,68 @@ class Notifications extends CI_Controller {
         authenticate_cookie();
         try_to_unban();
     }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Handle requests for deleting notifications.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function delete($ajax = FALSE)
+    {
+        log_access('notifications', 'delete');
+
+        $notification_id = $this->input->post('notification_id');
+
+        if ($this->_delete($notification_id))
+        {
+            $response['success'] = TRUE;
+        }
+
+        else
+        {
+            $response['success'] = FALSE;
+        }
+
+        if ( ! $ajax)
+        {
+            proceed('/');
+        }
+
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($response));
+    }
+
+    /**
+     * Delete a notification.
+     *
+     * @access   private
+     * @param    int       Notification id
+     * @return   bool
+     */
+    private function _delete($id)
+    {
+        if ( ! ($user = $this->session->userdata('user')))
+        {
+            return FALSE;
+        }
+
+        if ( ! ($notification = $this->notification->read($id)))
+        {
+            return FALSE;
+        }
+
+        if ( ! ($notification[0]['user_id'] == $user['user_id']))
+        {
+            return FALSE;
+        }
+
+        $this->notification->delete($id);
+
+        return TRUE;
+    }
 }
 
 /* End of file notifications.php */
