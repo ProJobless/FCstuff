ae1cb31b005df2285624022fcbe5217d1c4acf26
Abhijit Parida <abhijitrucks@gmail.com>
2013-09-01 09:48:19 +0530

    Add Accept method to Friends controller

--- application/controllers/friends.php
+++ application/controllers/friends.php
@@ -105,6 +105,86 @@ class Friends extends CI_Controller {
 
         return TRUE;
     }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Handle requests for accepting friend requests.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function accept($ajax = FALSE)
+    {
+        log_access('friends', 'accept');
+
+        $friend_user_id = $this->input->post('friend_user_id');
+
+        if ($this->_accept($friend_user_id))
+        {
+            $response['success'] = TRUE;
+        }
+
+        else
+        {
+            $response['success'] = FALSE;
+        }
+
+        if ( ! $ajax)
+        {
+            proceed('people/' . $friend_user_id);
+        }
+
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($response));
+    }
+
+    /**
+     * Accept a friend request.
+     *
+     * @access   private
+     * @param    int       Friend user id
+     * @return   bool
+     */
+    private function _accept($friend_user_id)
+    {
+        if ( ! ($user = $this->session->userdata('user')))
+        {
+            return FALSE;
+        }
+
+        if ($user['type'] == 'banned')
+        {
+            return FALSE;
+        }
+
+        if ( ! ($friend = $this->user->read($friend_user_id)))
+        {
+            return FALSE;
+        }
+
+        $relationship = $this->friend->read($user['user_id'], $friend_user_id);
+
+        if ( ! isset($relationship[0]))
+        {
+            return FALSE;
+        }
+
+        if ( ! ($relationship[0]['status'] == 'req_received'))
+        {
+            return FALSE;
+        }
+
+        $this->friend->update($user['user_id'], $friend_user_id, array(
+            'status' => 'friends'
+        ));
+
+        $this->friend->update($friend_user_id, $user['user_id'], array(
+            'status' => 'friends'
+        ));
+
+        return TRUE;
+    }
 }
 
 /* End of file friends.php */
