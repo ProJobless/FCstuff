b1e4306d4ae7be562a333585da1cac48b10f5471
Abhijit Parida <abhijitrucks@gmail.com>
2013-09-01 09:56:22 +0530

    Add Delete method to Friends controller

--- application/controllers/friends.php
+++ application/controllers/friends.php
@@ -185,6 +185,77 @@ class Friends extends CI_Controller {
 
         return TRUE;
     }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Handle requests for deleting a friend.
+     *
+     * @access   public
+     * @param    string
+     */
+    public function delete($ajax = FALSE)
+    {
+        log_access('friends', 'delete');
+
+        $friend_user_id = $this->input->post('friend_user_id');
+
+        if ($this->_delete($friend_user_id))
+        {
+            $response['success'] = TRUE;
+        }
+
+        else
+        {
+            $response['success'] = FALSE;
+        }
+
+        if ( ! $ajax)
+        {
+            proceed('/');
+        }
+
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($response));
+    }
+
+    /**
+     * Delete a friend.
+     *
+     * @access   private
+     * @param    int       Friend user id
+     * @return   bool
+     */
+    private function _delete($friend_user_id)
+    {
+        if ( ! ($user = $this->session->userdata('user')))
+        {
+            return FALSE;
+        }
+
+        if ($user['type'] == 'banned')
+        {
+            return FALSE;
+        }
+
+        if ( ! ($friend = $this->user->read($friend_user_id)))
+        {
+            return FALSE;
+        }
+
+        $relationship = $this->friend->read($user['user_id'], $friend_user_id);
+
+        if ( ! isset($relationship[0]))
+        {
+            return FALSE;
+        }
+
+        $this->friend->delete($user['user_id'], $friend_user_id);
+
+        $this->friend->delete($friend_user_id, $user['user_id']);
+
+        return TRUE;
+    }
 }
 
 /* End of file friends.php */
